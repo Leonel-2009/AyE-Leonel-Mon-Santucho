5064e0ec1e7d407884b0548fcb651f83479030ed
Leonel Mon Santucho <leonelmonsantucho@gmail.com>
2026-09-03 09:44:57 -0300

    Add files via upload

---
+++ Personaje.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Personaje
+    {
+        public int vidaTotal { get; set; }
+        public int vidaActual { get; set; }
+        public string UltimaAccion { get; set; }
+
+        public Personaje(int VidaT, int VidaA, string Accion)
+        {
+            vidaTotal = VidaT;
+            vidaActual = VidaA;
+            UltimaAccion = Accion;
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,66 @@
+﻿namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Evaluacion");
+            Console.WriteLine("");
+
+            Stack<Personaje> historialDelPersonaje = new Stack<Personaje>();
+
+            Personaje person = new Personaje(100, 100, "Caminar");
+            Personaje person2 = new Personaje(100, 100, "Correr");
+            Personaje person3 = new Personaje(100, 100, "Saltar");
+            Personaje person4 = new Personaje(100, 100, "Dormir");
+
+            historialDelPersonaje.Push(person);
+            historialDelPersonaje.Push(person2);
+            historialDelPersonaje.Push(person3);
+            historialDelPersonaje.Push(person4);
+
+            foreach (Personaje m in historialDelPersonaje)
+            {
+                Console.WriteLine($"- vida Actual: {m.vidaTotal} | Vida Total: {m.vidaActual} | Acción realizada: {m.UltimaAccion}");
+            }
+
+            Console.WriteLine("");
+            VolverEnElTiempo(historialDelPersonaje);
+            Console.WriteLine("");
+            Golpear(historialDelPersonaje);
+        }
+
+        static Personaje VolverEnElTiempo(Stack<Personaje> historialDelPersonaje)
+        {
+            var retroceder = historialDelPersonaje.Pop();
+
+            Console.WriteLine($"Accion borrada: {retroceder.UltimaAccion}");
+            Console.WriteLine("");
+
+            Console.WriteLine("Historial actualizado");
+            foreach (var m in historialDelPersonaje)
+            {
+                Console.WriteLine($"- {m.UltimaAccion}");
+            }
+
+            return retroceder;
+        }
+        static Personaje Golpear(Stack<Personaje> historialDelPersonaje)
+        {
+            var daño = 20;
+
+            Personaje person = new Personaje(100 , 100 - daño, "Golpe recibido");
+
+            historialDelPersonaje.Push(person);
+
+            Console.WriteLine("");
+            Console.WriteLine("Historial actualizado:");
+            foreach (var m in historialDelPersonaje)
+            {
+                Console.WriteLine($"- vida Actual: {m.vidaActual} | Vida Total: {m.vidaTotal} | Acción realizada: {m.UltimaAccion}");
+            }
+
+            return person;
+        }
+    }
+}
