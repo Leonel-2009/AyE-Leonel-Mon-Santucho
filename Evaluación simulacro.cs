c08f0d5c0eb7f7bb9089c358a21e193c04dba468
Leonel Mon Santucho <leonelmonsantucho@gmail.com>
2026-09-02 22:19:28 -0300

    Add files via upload

---
+++ Evaluación simulacro.cs
@@ -0,0 +1,36 @@
+﻿namespace Evaluacion_simulacro
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<Ubicacion> historialUbicaciones = new Stack<Ubicacion>();
+
+            Ubicacion Ubication = new Ubicacion(120, 30, "Bosque");
+            Ubicacion Ubication2 = new Ubicacion(340, 120, "Selva");
+            Ubicacion Ubication3 = new Ubicacion(-230, 20, "Pantano");
+
+            historialUbicaciones.Push(Ubication);
+            historialUbicaciones.Push(Ubication2);
+            historialUbicaciones.Push(Ubication3);
+
+            Console.WriteLine("\n Ubicaciones buscadas:");
+
+            var eliminado = historialUbicaciones.Pop();
+
+            foreach (Ubicacion Url in historialUbicaciones)
+            {
+                Console.WriteLine($"- Coordenadas: {Url.X} ; {Url.Y} | Nombre: {Url.NombreZona}");
+            }
+
+            Console.WriteLine("\n Ubicación eliminada:");
+            
+           
+            Console.WriteLine($"{eliminado}");
+                
+
+
+            
+        }
+    }
+}
