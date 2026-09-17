1e73bc42e51f815d8e484e97a7d47f4b280de7dc
Leonel Mon Santucho <leonelmonsantucho@gmail.com>
2026-09-17 12:05:58 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Puntos2D puntitos = new Puntos2D(60, 48);
+            Puntos2D puntitos2 = new Puntos2D(10, 70);
+            Puntos2D puntitos3 = new Puntos2D(50, 50);
+
+            puntitos.Mostrar();
+            puntitos2.Mostrar();
+            puntitos3.Mostrar();
+        }
+    }
+}
