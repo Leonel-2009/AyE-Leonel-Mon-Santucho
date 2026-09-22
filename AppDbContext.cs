0eeb85607162295a1a8f585d6fadce63d8b3e2c8
Leonel Mon Santucho <leonelmonsantucho@gmail.com>
2026-09-22 13:28:00 -0300

    Add files via upload

---
+++ AppDbContext.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace ConsoleApp4
+{
+    public class Productos
+    {
+        public int ID { get; set; }
+        public string Nombre { get; set; }
+        public int Codigo { get; set; }
+        public double Precio { get; set; }
+    }
+
+    internal class AppDbContext : DbContext
+    {
+        public DbSet<Productos> Productos => Set<Productos>();
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            string connectionString = "Server=localhost;Port=3307;Database=tp_n23;Uid=root;pwd=;";
+            var serverVersion = ServerVersion.AutoDetect(connectionString);
+            optionsBuilder.UseMySql(connectionString, serverVersion);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+        }
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿namespace ConsoleApp4
+{
+    internal class Program
+    {
+        public struct Producto
+        {
+            public string Nombre { get; set; }
+            public int Codigo { get; set; }
+            public double Precio { get; set; }
+        }
+        static void Main(string[] args)
+        {
+            Producto[] inventario = new Producto[3];
+
+            inventario[0] = new Producto { Nombre = "Patito de Hule + tres patitos chiquitos", Codigo = 7896, Precio = 5900.52 };
+            inventario[1] = new Producto { Nombre = "TV 65 pulgadas", Codigo = 9876, Precio = 9900000 };
+            inventario[2] = new Producto { Nombre = "Optimus Prime", Codigo = 10223, Precio = 100000 };
+
+            foreach (Producto producto in inventario)
+            {
+                Console.WriteLine($"Producto: {producto.Nombre} | Codigo: {producto.Codigo} | Precio: {producto.Precio}");
+            }
+
+            using var db = new AppDbContext();
+
+            foreach(Producto p in inventario)
+            {
+                Productos productitos = new Productos
+                {
+                    Nombre = p.Nombre,
+                    Codigo = p.Codigo,
+                    Precio = p.Precio
+                };
+
+                db.Productos.Add(productitos);
+
+                db.SaveChanges();
+            }
+        }
+    }
+}
